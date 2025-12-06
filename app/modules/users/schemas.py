62a29ca6b41685cf9850b33303582b47c568f524
romaperec <cpser621@gmail.com>
2025-12-06 16:09:18 +0300

    Add create and delete user methods

--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
 
 
+class UserCreate(BaseModel):
+    id: int
+    username: str
+    email: str
+    hashed_password: str
+
+
 class UserResponse(BaseModel):
     id: int
     username: str
--- app/modules/users/service.py
+++ app/modules/users/service.py
@@ -6,7 +6,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.modules.users.models import User
-from app.modules.users.schemas import UserResponse
+from app.modules.users.schemas import UserCreate, UserResponse
 
 
 class UserService:
@@ -46,3 +46,33 @@ class UserService:
             username=existing_user.username,
             email=existing_user.email,
         )
+
+    async def create_user(self, schema: UserCreate):
+        new_user = User(
+            username=schema.username,
+            email=schema.email,
+            password=schema.hashed_password,
+        )
+        self.db.add(new_user)
+        await self.db.commit()
+
+        logger.info(f"Был создан пользователь с email: {new_user.email}.")
+        return UserResponse(
+            id=new_user.id, username=new_user.username, email=new_user.email
+        )
+
+    async def delete_user(self, id: int):
+        existing_user = await self.db.execute(select(User).where(User.id == id))
+        existing_user = existing_user.scalar_one_or_none()
+
+        if existing_user is None:
+            logger.warning(f"Пользователь с id: {id} не был найден в базе данных.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found by id"
+            )
+
+        await self.db.delete(existing_user)
+        await self.db.commit()
+
+        logger.info(f"Пользователь с id: {id} был удален.")
+        return {"status": "success", "deleted_id": id}
