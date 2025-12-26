8f69448a57dc25f83e92d3674018a015452ea5bd
romaperec <cpser621@gmail.com>
2025-12-26 20:53:46 +0300

    Upgrade performance db

--- app/core/db_helper.py
+++ app/core/db_helper.py
@@ -7,7 +7,15 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-engine = create_async_engine(url=settings.DB_URL, echo=settings.DB_ECHO)
+engine = create_async_engine(
+    url=settings.DB_URL,
+    echo=settings.DB_ECHO,
+    pool_size=15,
+    max_overflow=5,
+    pool_timeout=30,
+    pool_recycle=3600,
+    pool_pre_ping=True,
+)
 sessionmaker = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
