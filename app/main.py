882e9d029d4338c9df08c346f8de26bcc9b50145
romaperec <cpser621@gmail.com>
2025-12-27 15:27:57 +0300

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 
+from app.core.config import settings
 from app.core.redis import lifespan
 from app.modules.auth.router import router as auth_router
 from app.modules.users.router import router as users_router
 
 app = FastAPI(title="CodeVenture API", lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.FRONTEND_URL,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(users_router)
 
